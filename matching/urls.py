c64762e811e29facb13882eaf00fa019f6bbd590
Arman <arman1381keshazar@gmail.com>
2024-07-04 18:35:08 +0330

    getVerifiersView and url_patterns added

--- matching/urls.py
+++ matching/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 from matching import views
 
 urlpatterns = [
-    path("getVerifiers/<int: post_token>", views.GetVerifiersView.as_view(), name="get_verifiers")
+    path("getVerifiers/<int:post_token>", views.GetVerifiersView.as_view(), name="get_verifiers")
 ]
--- matching/views.py
+++ matching/views.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from accounts import models as account_models
+from accounts import serializers as account_serializers
 
 
 class GetVerifiersView(APIView):
@@ -12,8 +13,6 @@ class GetVerifiersView(APIView):
         except account_models.Post.DoesNotExist:
             return Response("error: Post not found", status=status.HTTP_404_NOT_FOUND)
         verifiers = post.selected_verifiers.all()
+        serializer = account_serializers.VerifierSerializer(verifiers, many=True)
+        return Response(data={"verifiers": serializer.data}, status=status.HTTP_200_OK)
 
-        pass
-
-
-# Create your views here.
