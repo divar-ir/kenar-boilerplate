151920f4f7411ed5b7506d378362896c5cc9ae24
sepehrmnp <sepehr.mirnasrollahi@aut.ac.ir>
2024-07-04 22:25:19 +0330

    added addons function

--- matching/views.py
+++ matching/views.py
@@ -6,6 +6,29 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from accounts import models as account_models
 from accounts import serializers as account_serializers
+from boilerplate.clients import get_divar_kenar_client
+
+from kenar import (
+    CreatePostAddonRequest,
+    GetUserAddonsRequest,
+    DeleteUserAddonRequest,
+    GetPostAddonsRequest,
+    DeletePostAddonRequest,
+    CreateUserAddonRequest,
+    IconName,
+    Icon,
+    TitleRow,
+    SubtitleRow,
+    SelectorRow,
+    ScoreRow,
+    LegendTitleRow,
+    GroupInfo,
+    EventRow,
+    EvaluationRow,
+    DescriptionRow,
+    Color,
+    WideButtonBar,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -38,3 +61,36 @@ class SetVerifiersView(APIView):
         except Exception as e:
             logger.error(f"set verification error {e}")
             return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+def add_addons(access_token, post_token, verifiers):
+    addons = []
+    kenar_client = get_divar_kenar_client()
+    for i in range(min(3, len(verifiers))):
+        addons.append(EventRow(
+            title=f"{verifiers[i].firstname} {verifiers[i].lastname}",
+            subtitle=verifiers[i].rate,
+            has_indicator=False,
+            label="انتخاب",
+            has_divider=True,
+            link=f"https://salsa.darkube.app/select-verifier/{post_token}?verifier-id={verifiers[i].pk}",
+            padded=True,
+            icon=Icon(icon_name=IconName.ADD),
+            )
+        )
+    addons.append(
+        WideButtonBar(
+            button=WideButtonBar.Button(
+                title="گزینه های بیشتر ..." if len(verifiers)>3 else "لیست کامل", link="https://salsa.darkube.app/select-verifier/{post_token}"
+            ),
+        )
+    )
+
+    resp = kenar_client.addon.create_post_addon(
+            access_token=access_token,
+            data=CreatePostAddonRequest(
+                token=post_token,
+                widgets=addons,
+            ),
+        )
+    
+    logger.info(resp)
