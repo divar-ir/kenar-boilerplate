9e5b2be8a22b885f380895b8c07b4b71b5a9fc72
Amirsalar Safaei <amirsalar.safaei@divar.ir>
2024-07-04 18:45:46 +0330

    (bump) kenar sdk(fix breakings)

--- boilerplate/clients.py
+++ boilerplate/clients.py
@@ -1,10 +1,10 @@
 from functools import lru_cache
 
-from kenar.app import KenarApp
+from kenar import Client
 
 from boilerplate import settings
 
 
 @lru_cache(maxsize=1)
-def get_divar_kenar_client() -> KenarApp:
-    return KenarApp(settings.divar_kenar_client_conf)
+def get_divar_kenar_client() -> Client:
+    return Client(settings.divar_kenar_client_conf)
